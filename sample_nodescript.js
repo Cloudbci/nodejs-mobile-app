1bb2657a1d677ea57975d7b029016596b1f4fb48
unknown <Administrator@SSSBG4NBL513.hdfc.com>
2024-03-06 15:57:35 +0530

    Initial commit Add files

---
+++ sample_nodescript.js
@@ -0,0 +1,8 @@
+const config = require('./config');
+
+const { NAME, PORT, SESSION_SECRET } = config;
+
+console.log('NAME:', NAME);
+console.log('PORT:', PORT)
+console.log('SESSION_SECRET:', SESSION_SECRET)
+
